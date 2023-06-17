4e428d9d9f0087c8a1d11f5a91afea8a0659e9f1
Mariano1601 <mariano.moran@correo.unimet.edu.ve>
2023-06-17 18:23:24 -0400

    Draw function with networkX

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from classes.graph import Graph
 from scripts.csv import *
 import matplotlib.pyplot as plt
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def draw_graph(graph):
@@ -37,6 +39,27 @@ def draw_graph(graph):
     plt.show()
 
 
+def drawGraphNX(graph: Graph):
+    g = nx.Graph()
+    # Agregar nodos y aristas al grafo
+    for node, neighbors in graph.graph.items():
+        for neighbor, cost in neighbors:
+            g.add_edge(node, neighbor)
+
+    # Dibujar el grafo
+    nx.draw(g, with_labels=True, node_color='lightblue', node_size=500,
+            font_size=10, edge_color='gray', width=1, alpha=0.7)
+
+    # Etiquetas de las aristas
+    labels = nx.get_edge_attributes(g, 'weight')
+    nx.draw_networkx_edge_labels(
+        g, pos=nx.spring_layout(g, seed=60), edge_labels=labels)
+
+    # Mostrar el gráfico
+    plt.show()
+    return
+
+
 def main():
     graph = Graph()
     getNodesData(graph)
@@ -44,25 +67,31 @@ def main():
 
     graph.printGraph()
 
+    drawGraphNX(graph)
+
     print("\n")
 
-    draw_graph(graph.graph)
+    # draw_graph(graph.graph)
+
     # startNode = 'PTP'
     # end_node = 'CCS'
+    # hasVisa = False
 
     # shortest_distance, shortest_path = graph.findShortestPath(
-    #     startNode, end_node, True)
+    #     startNode, end_node, hasVisa)
 
     # print(
     #     f"La distancia más corta desde {startNode} hasta {end_node} es: {shortest_distance}")
     # print(f"El camino más corto es: {shortest_path} \n")
 
     # shortest_stop_path = graph.findShortestStopPath(
-    #     startNode, end_node, True)
+    #     startNode, end_node, hasVisa)
 
     # print(
     #     f"La menor cantidad de escalas desde {startNode} hasta {end_node} es: {len(shortest_stop_path) - 2}")
     # print(f"El camino con menor cantidad de escalas es: {shortest_stop_path}")
+
+    # Crear el objeto de grafo
 
 
 if __name__ == '__main__':
